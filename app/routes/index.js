6e83438f65ae9f604b059cc800cb89665c127a13
Austin Minnon and Joshua Gustafson <whi7ee@gmail.com>
2016-03-30 15:59:49 -0700

    adds update feature

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,16 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+    updatePostForm(post, params) {
+          Object.keys(params).forEach(function(key) {
+            if(params[key]!==undefined) {
+              post.set(key,params[key]);
+            }
+          });
+          post.save();
+          this.transitionTo('index');
+        },
+
     destroyPost(post) {
       post.destroyRecord();
       this.transitionTo('index');
